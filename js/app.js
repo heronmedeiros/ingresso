97c1a7f64ea2ef3d575222958945ed8f44998259
Heron Medeiros <heron.medeiros@gmail.com>
2023-09-30 13:55:00 -0300

    refactoring

--- js/app.js
+++ js/app.js
@@ -2,51 +2,50 @@ const tipoPista    = (tipo) => tipo == 'pista';
 const tipoInferior = (tipo) => tipo == 'inferior';
 const tipoSuperior = (tipo) => tipo == 'superior';
 
+const parseQtd = elementQtd => parseInt(elementQtd);
+
+const compraQntInferior = tipo => alert(`Quantidade indisponível para tipo ${tipo}`);
+const compraQntSuperior = (quantidade, elementQtd) => {
+  let qtd = parseInt(elementQtd.textContent);
+
+  qtd = qtd - quantidade;
+  elementQtd.textContent = qtd;
+  alert('Compra realizada com sucesso');
+}
+
+const compra = (quantidade, tipo, id) => {
+  let elementQtd = document.getElementById(id);
+
+  if(quantidade > parseQtd(elementQtd.textContent) ) {
+    compraQntInferior(tipo);
+  } else {
+    compraQntSuperior(quantidade, elementQtd);
+  }
+}
 
 const comprarPista = (quantidade) => {
-  let qtdPistaElement = document.getElementById('qtd-pista');
-  let qtdPista = parseInt(qtdPistaElement.textContent);
-
-  if(quantidade > qtdPista ) {
-    alert('Quantidade indisponível para tipo pista');
-  } else {
-    qtdPista = qtdPista - quantidade;
-    qtdPistaElement.textContent = qtdPista;
-    alert('Compra realizada com sucesso');
-  }
+  const id = 'qtd-pista';
+  const tipo = 'pista';
+  compra(quantidade, tipo, id);
 }
 
 const compraInferior = (quantidade) => {
-  let qtdInferiorElement = document.getElementById('qtd-inferior');
-  let qtdInferior = parseInt(qtdInferiorElement.textContent);
-
-  if(quantidade > qtdInferior ) {
-    alert('Quantidade indisponível para tipo Inferior');
-  } else {
-    qtdInferior = qtdInferior - quantidade;
-    qtdInferiorElement.textContent = qtdInferior;
-    alert('Compra realizada com sucesso');
-  }
+  const id = 'qtd-inferior';
+  const tipo = 'inferior';
+  compra(quantidade, tipo, id);
 }
 
 const compraSuperior = (quantidade) => {
-  let qtdSuperiorElement = document.getElementById('qtd-superior');
-  let qtdSuperior = parseInt(qtdSuperiorElement.textContent);
-
-  if(quantidade > qtdSuperior ) {
-    alert('Quantidade indisponível para tipo Superior');
-  } else {
-    qtdSuperior = qtdSuperior - quantidade;
-    qtdSuperiorElement.textContent = qtdSuperior;
-    alert('Compra realizada com sucesso');
-  }
+  const id = 'qtd-superior';
+  const tipo = 'superior';
+  compra(quantidade, tipo, id);
 }
+
 function comprar() {
   let tipo = document.getElementById('tipo-ingresso');
   let quantidade = parseInt(document.getElementById('qtd').value);
 
-
-  if(tipoPista(tipo.value)) {
+  if (tipoPista(tipo.value)) {
     comprarPista(quantidade);
   } else if(tipoInferior(tipo.value)) {
     compraInferior(quantidade);
